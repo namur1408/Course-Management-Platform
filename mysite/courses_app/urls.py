f74308b2e6597224bbee548ba9dbc6aa8f42b65a
oleg_androsiuk <a.oleg1408@gmail.com>
2025-11-05 22:50:33 +0100

    Changing FBV into CBV

--- mysite/courses_app/urls.py
+++ mysite/courses_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import course_list, create_course, enroll_course, delete_course
+from .views import enroll_course, CourseListView, CourseCreateView, CourseDeleteView
 
 urlpatterns = [
-    path('', course_list, name='course_list'),
-    path('create/', create_course, name='create_course'),
+    path('', CourseListView.as_view(), name='course_list'),
+    path('create/', CourseCreateView.as_view(), name='create_course'),
     path('<int:course_id>/enroll/', enroll_course, name='enroll_course'),
-    path('<int:course_id>/delete/', delete_course, name='delete_course'),
+    path('<int:pk>/delete/', CourseDeleteView.as_view(), name='delete_course'),
 ]
--- mysite/courses_app/views.py
+++ mysite/courses_app/views.py
@@ -2,27 +2,26 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Course
 from .forms import CourseForm
 from django.contrib.auth.decorators import login_required, permission_required
+from django.views.generic import ListView, CreateView, DeleteView
 from members_app.models import Member
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse_lazy
 
-@login_required
-def course_list(request):
-    courses = Course.objects.all()
-    return render(request, 'courses/course_list.html', {'courses': courses})
+class CourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'courses/course_list.html'
+    context_object_name = 'courses'
 
-@login_required
-def create_course(request):
-    if request.method == 'POST':
-        form = CourseForm(request.POST)
-        if form.is_valid():
-            course = form.save(commit=False)
-            course.creator = request.user
-            course.save()
-            form.save_m2m()
-            return redirect('course_list')
-    else:
-        form = CourseForm()
-    return render(request, 'courses/create_course.html', {'form': form})
+class CourseCreateView(LoginRequiredMixin, CreateView):
+    model = Course
+    form_class = CourseForm
+    template_name = 'courses/create_course.html'
+    context_object_name = 'courses'
+    success_url = reverse_lazy('course_list')
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
 
 @login_required
 def enroll_course(request, course_id):
@@ -30,13 +29,13 @@ def enroll_course(request, course_id):
     course.members.add(request.user)
     return redirect('course_list')
 
-@login_required
-def delete_course(request, course_id):
-    course = get_object_or_404(Course, id=course_id)
-    if course.creator != request.user and not request.user.has_perm('courses_app.can_delete_courses'):
-        return redirect('course_list')
-    if request.method == 'POST':
-        course.delete()
-        return redirect('course_list')
-    return render(request, 'courses/course_confirm_delete.html', {'course': course})
+class CourseDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    model = Course
+    template_name = 'courses/course_confirm_delete.html'
+    context_object_name = 'course'
+    success_url = reverse_lazy('course_list')
+    permission_required = 'courses_app.can_delete_course'
 
+    def has_permission(self):
+        course = self.get_object()
+        return (course.creator == self.request.user or self.request.user.has_perm('courses_app.can_delete_course'))
